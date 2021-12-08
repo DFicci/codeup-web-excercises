ed142ca01366ca5c8ca9d433e61d41aa04d23958
Daniel V Ficci <danielficci@gmail.com>
2021-12-08 11:59:22 -0600

    adds hello asteroid

---
+++ js/hello-asteroid.js
@@ -0,0 +1,15 @@
+// Describing my favorite food to eat
+console.log("My favorite food to eat " + "is al pastor street tacos. " + "The place i go to eat them is at 100% taco." +
+    " Cannot forget salsa, onion, and pineapples.");
+
+//Favorite breakfast
+    var food = "My favorite breakfast is ";
+    var mcd = "Bacon, egg and cheese McGriddle from Mcdonald's";
+console.log(food + mcd);
+
+//But I lied
+    food = "I lied though, i just like to eat cereal. ";
+    mcd = "Either Cheerios or Reese's Puffs ";
+console.log(food + mcd);
+
+
