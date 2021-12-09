e2f7375e1e19e27e1b5af0317217134b14202abf
Daniel V Ficci <danielficci@gmail.com>
2021-12-08 22:24:38 -0600

    excercies

---
+++ js/Excercises.js
@@ -0,0 +1,43 @@
+// Write javascript code, that is, variables and operators to describe the following scenarios.
+// You rented 3 movies and the price is $3 per day.
+var mermaid = 3, bear = 5, hercules = 1;
+//addition
+var sum = (mermaid + bear + hercules);
+//total
+var n = sum * 3
+console.log("In order to watch all 3 movies with your kids, \nYou would have to pay a total of " + "$" + n + " for the movies.");
+
+//Working as a contractor for Google, Amazon, and Facebook
+//hours worked
+var google = 6, amazon = 4, fb = 10;
+//hourly wage, h = hourly rate
+var gh = 400, amazonh = 380, fbh = 350;
+//addition
+var total = (google+gh) + (amazon+amazonh) + (fb+fbh);
+//total amount made
+console.log("\nThe total amount of money you would make as a contractor for all three companies is " + "$" + total);
+
+// if a student tries to enroll in class
+var student = 1, enrolled = 23, course = student + enrolled;
+var message;
+if (course > 25) {
+    message = "\nSorry, please choose a different class. All seats are filled."
+} else if (course < 25 ) {
+    message = "\nCongratulations on your enrollment into the class!"
+};
+console.log(message)
+
+//Product discount
+var product = 5;
+var discount = .20;
+var items = 2;
+var price = 5;
+var equal;
+var pay;
+if (product > items){
+    pay = (price * discount), equal = price - pay
+} else if (product < items){
+    equal = price
+};
+console.log("\nSince you bought 5 items you received a 20 percent" +
+    " discount, your new total is $" + equal + ".")
--- js/hello-asteroid.js
+++ js/hello-asteroid.js
@@ -3,13 +3,12 @@ console.log("My favorite food to eat " + "is al pastor street tacos. " + "The pl
     " Cannot forget salsa, onion, and pineapples.");
 
 //Favorite breakfast
-    var food = "My favorite breakfast is ";
-    var mcd = "Bacon, egg and cheese McGriddle from Mcdonald's";
+    var food = "My favorite breakfast is " , mcd = "Bacon, egg and cheese McGriddle from Mcdonald's";
 console.log(food + mcd);
 
 //But I lied
-    food = "I lied though, i just like to eat cereal. ";
-    mcd = "Either Cheerios or Reese's Puffs ";
+    food = "I lied though, i just like to eat cereal. \n";
+    mcd = "Either \n\t\t\tCheerios \n\t\tor \n\tReese's \nPuffs ";
 console.log(food + mcd);
 
 
