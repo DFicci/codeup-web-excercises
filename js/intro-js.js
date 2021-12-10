8783b38bfd1ac309aa75b85fe55ae49b8f3e4f1b
Daniel V Ficci <danielficci@gmail.com>
2021-12-10 09:55:01 -0600

    Sectioned of problems,In class excercise

--- js/intro-js.js
+++ js/intro-js.js
@@ -6,7 +6,7 @@ let sum = (mermaid + bear + hercules);
 //total
 let n = sum * 3
 console.log("In order to watch all 3 movies with your kids, \nYou would have to pay a total of " + "$" + n + " for the movies.");
-
+//----------------------------------------------------------------------------------------------------------------------
 //Working as a contractor for Google, Amazon, and Facebook.
 //hours worked.
 let google = 6, amazon = 4, fb = 10;
@@ -16,7 +16,7 @@ let gh = 400, amazonh = 380, fbh = 350;
 let total = (google * gh) + (amazon * amazonh) + (fb * fbh);
 //total amount made
 console.log("\nThe total amount of money you would make as a contractor for all three companies is " + "$" + total);
-
+//----------------------------------------------------------------------------------------------------------------------
 // if a student tries to enroll in class
 let student = 1, enrolled = 25, course = student + enrolled;
 //Message to be relayed when enrolling in class
@@ -27,7 +27,7 @@ if (course >= 26) {
     message = "\nCongratulations on your enrollment into the class!"
 }
 console.log(message)
-
+//----------------------------------------------------------------------------------------------------------------------
 //Product discount
 //How many products?
 let product = 1;
@@ -49,7 +49,7 @@ if (product >= items) {
     equal = price
     console.log("\nWe have a discount today, would you like to buy another item to recieve that disount.")
 }
-
+//----------------------------------------------------------------------------------------------------------------------
 //username and password
 let username = 'codeup';
 let password = 'notastrongpassword';
@@ -71,7 +71,7 @@ if ((password.includes(user)) && (username.length > 20) && (password.length < 5)
 } else
     console.log("\nYour account has been successfully created, Welcome " + username + "!");{
 }
-
+//----------------------------------------------------------------------------------------------------------------------
 //excercise 2
 let sample = "\nhello codeup";
 console.log(sample.length);
@@ -82,3 +82,19 @@ console.log(sample.indexOf("c"));
 console.log(sample.indexOf("C"));
 // use substring first, then index of
 console.log(sample.substring(sample.indexOf("c"),sample.indexOf("p") + 1));
+
+// --------------------------------------------------------------------------------------------------------------------
+
+/*In class excercise
+*Using the provided variables, create a piece of logic which will determine if a person
+*is qualified to get an extended auto loan.
+*requirements: credit score must be over 680 and have at least $4000 on hand or, have at least $10000 on-hand
+*/
+
+let creditScore = 590;
+let cashOnHand = 12000;
+
+if ((creditScore > 680) && (cashOnHand >= 4000) || (cashOnHand >= 10000)){
+    console.log("Congratulations, you've been accepted into our extended auto loan program!")
+} else
+    console.log("Sorry to inform you that you do not meet our requirements for our auto loan program.")
