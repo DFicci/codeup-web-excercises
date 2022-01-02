77a45205476cd2567028974a54ea7c4151eab4a1
Daniel V Ficci <danielficci@gmail.com>
2022-01-02 15:14:30 -0600

    added comments

--- js/Prime_number_challenge.js
+++ js/Prime_number_challenge.js
@@ -1,11 +1,12 @@
+// Function to determine whether number is prime or not
 function isPrime(num) {
     for(var i = 2; i < num; i++)
         if(num % i === 0) return false;
     return num > 1;
 }
-
 let counter = 0;
 let num = 1;
+//If number is prime will add one to counter, once counter reaches 50 it will stop.
 while (counter <=50){
     if (isPrime(num)){
         console.log(num);
