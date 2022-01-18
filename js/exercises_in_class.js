20ce16b03c73dfe4844102ae8f4ad269914feb94
Daniel V Ficci <danielficci@gmail.com>
2022-01-18 10:25:09 -0600

    flex-form

--- js/exercises_in_class.js
+++ js/exercises_in_class.js
@@ -113,27 +113,27 @@
 //   }
 //  }
 // }---------------------------
-//cones started with
-// var allCones = Math.floor(Math.random() * 50) + 50;
-// var counter = 0;
-// //
-// do {
-//  counter++;
-//  console.log("i sold " + counter + " cones.");
-//  console.log("there are " + allCones + " cones available");
-//  console.log("cones that I tried to sell last time ", counter);
-//  var randomNum = Math.floor(Math.random() * 5) + 1;
+// cones started with
+var allCones = Math.floor(Math.random() * 50) + 50;
+var counter = 0;
 //
-//  if (randomNum > allCones) {
-//   console.log("I cannot sell you " + counter + ", I only have " + allCones + " left.")
-//  } else {
-//   allCones -= randomNum;
-//   console.log("I sold " + counter + " cones.");
-//  }
-// }
-//
-// while (allCones > 0);
-// console.log("Yay, i am all sold out of my cones")
+do {
+ counter++;
+ console.log("i sold " + counter + " cones.");
+ console.log("there are " + allCones + " cones available");
+ console.log("cones that I tried to sell last time ", counter);
+ var randomNum = Math.floor(Math.random() * 5) + 1;
+
+ if (randomNum > allCones) {
+  console.log("I cannot sell you " + counter + ", I only have " + allCones + " left.")
+ } else {
+  allCones -= randomNum;
+  console.log("I sold " + counter + " cones.");
+ }
+}
+
+while (allCones > 0);
+console.log("Yay, i am all sold out of my cones")
 
 // TODO: MINI EXERCISE
 //      Write a function which uses a for-loop and
--- js/while.js
+++ js/while.js
@@ -1,5 +1,21 @@
-var power = 1;
-while(power <= 16){
-    console.log(Math.pow(2, power));
-    power++
+// cones started with
+var allCones = Math.floor(Math.random() * 50) + 50;
+var counter = 0;
+//
+do {
+    counter++;
+    console.log("i sold " + counter + " cones.");
+    console.log("there are " + allCones + " cones available");
+    console.log("cones that I tried to sell last time ", counter);
+    var randomNum = Math.floor(Math.random() * 5) + 1;
+
+    if (randomNum > allCones) {
+        console.log("I cannot sell you " + counter + ", I only have " + allCones + " left.")
+    } else {
+        allCones -= randomNum;
+        console.log("I sold " + counter + " cones.");
+    }
 }
+
+while (allCones > 0);
+console.log("Yay, i am all sold out of my cones")
