4410375b8a83f1f070762288c172e99761ff070f
Daniel V Ficci <danielficci@gmail.com>
2022-01-26 09:26:18 -0600

    morning exercise

--- js/exercises_in_class.js
+++ js/exercises_in_class.js
@@ -113,27 +113,27 @@
 //   }
 //  }
 // }---------------------------
-// cones started with
-var allCones = Math.floor(Math.random() * 50) + 50;
-var counter = 0;
+// // cones started with
+// var allCones = Math.floor(Math.random() * 50) + 50;
+// var counter = 0;
+// //
+// do {
+//  counter++;
+//  console.log("i sold " + counter + " cones.");
+//  console.log("there are " + allCones + " cones available");
+//  console.log("cones that I tried to sell last time ", counter);
+//  var randomNum = Math.floor(Math.random() * 5) + 1;
 //
-do {
- counter++;
- console.log("i sold " + counter + " cones.");
- console.log("there are " + allCones + " cones available");
- console.log("cones that I tried to sell last time ", counter);
- var randomNum = Math.floor(Math.random() * 5) + 1;
-
- if (randomNum > allCones) {
-  console.log("I cannot sell you " + counter + ", I only have " + allCones + " left.")
- } else {
-  allCones -= randomNum;
-  console.log("I sold " + counter + " cones.");
- }
-}
-
-while (allCones > 0);
-console.log("Yay, i am all sold out of my cones")
+//  if (randomNum > allCones) {
+//   console.log("I cannot sell you " + counter + ", I only have " + allCones + " left.")
+//  } else {
+//   allCones -= randomNum;
+//   console.log("I sold " + counter + " cones.");
+//  }
+// }
+//
+// while (allCones > 0);
+// console.log("Yay, i am all sold out of my cones")
 
 // TODO: MINI EXERCISE
 //      Write a function which uses a for-loop and
@@ -328,7 +328,15 @@ console.log("Yay, i am all sold out of my cones")
 
 
 
+function averageOfNumbers(arrayOfNumbers) {
+ let sum = 0 ;
 
+  arrayOfNumbers.forEach(function (number){
+   sum = sum += number
+  });
+ return sum / arrayOfNumbers;
+}
+ console.log(averageOfNumbers([1,2,3,4,5,6]))
 
 
 
