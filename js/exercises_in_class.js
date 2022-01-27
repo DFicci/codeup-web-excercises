031a76fb7d0f420ddc1e53680ff06a22e3eeeb3c
Daniel V Ficci <danielficci@gmail.com>
2022-01-27 17:00:10 -0600

    updated dom-query-js

--- js/exercises_in_class.js
+++ js/exercises_in_class.js
@@ -328,18 +328,31 @@
 
 
 
-function averageOfNumbers(arrayOfNumbers) {
- let sum = 0;
+// function averageOfNumbers(arrayOfNumbers) {
+//  let sum = 0;
+//
+//   arrayOfNumbers.forEach(function (number){
+//       sum = sum + number;
+//   });
+//  return sum / arrayOfNumbers;
+// }
+//
+// console.log(averageOfNumbers([1,2,3,4,5,6]))
 
-  arrayOfNumbers.forEach(function (number){
-      sum = sum + number;
-  });
- return sum / arrayOfNumbers;
+
+// morning JS warmup:
+// write a function called sumOfSquares(a, b)
+// takes 2 parameters called a and b
+// returns the sum of the squares of a and b
+// i.e., (a to the power of 2) + (b to the power of 2)
+// e.g., console.log(sumOfSquares(2, 3));
+// will return 13 (edited)
+
+function sumOfSquares(a, b){
+    return console.log((a ** 2) + (b ** 2))
 }
 
-console.log(averageOfNumbers([1,2,3,4,5,6]))
-
-
+sumOfSquares(2,3)
 
 
 
